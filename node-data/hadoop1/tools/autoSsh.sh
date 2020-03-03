92a51eab72a1c134f9d5d84c4b34b5ce92ee15f9
xcq <xcq5120@gmail.com>
2020-03-03 20:09:13 +0800

    ssh免密配置的一键脚本，应置于数据卷中，由hadoop-1执行

---
+++ node-data/hadoop1/tools/autoSsh.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+SERVERS="hadoop-1 hadoop-2 hadoop-3" 
+PASSWORD=kfk 
+ 
+auto_ssh_copy_id() { 
+    expect -c "set timeout -1; 
+        spawn ssh-copy-id $1; 
+        expect { 
+            *(yes/no)* {send -- yes\r;exp_continue;} 
+            *assword* {send -- $2\r;exp_continue;} 
+            eof        {exit 0;} 
+        }"; 
+} 
+ 
+ssh_copy_id_to_all() { 
+    for SERVER in $SERVERS 
+    do 
+        auto_ssh_copy_id $SERVER $PASSWORD 
+    done 
+} 
+ 
+rm -rf /home/kfk/.ssh
+ssh-keygen -t rsa -f /home/kfk/.ssh/id_rsa -P "" 
+ssh_copy_id_to_all
