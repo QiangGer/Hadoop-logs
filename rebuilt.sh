1fd2d2d97434302cf97eb1d5e13dda1425d095b2
xcq <xcq5120@gmail.com>
2020-03-03 20:05:26 +0800

    一键摧毁正在运行的容器并删除镜像，重头来过

---
+++ rebuilt.sh
@@ -0,0 +1,30 @@
+#!bash
+
+cd /home/xcq/projects/Hadoop-news
+
+docker-compose stop
+
+docker-compose rm -f
+
+docker rmi hadoop
+
+# 删除日志
+rm -rf ./node-data/hadoop1/modules/hadoop-2.5.0/logs
+
+# 删除hdfs存储内容
+rm -rf ./node-data/hadoop1/modules/hadoop-2.5.0/data/tmp/*
+
+# 删除slave上的hadoop
+rm -rf ./node-data/hadoop2/modules/hadoop-2.5.0/*
+rm -rf ./node-data/hadoop3/modules/hadoop-2.5.0/*
+
+# 把主节点的hadoop拷贝过去
+cp -rf ./node-data/hadoop1/modules/hadoop-2.5.0/* ./node-data/hadoop2/modules/hadoop-2.5.0
+cp -rf ./node-data/hadoop1/modules/hadoop-2.5.0/* ./node-data/hadoop3/modules/hadoop-2.5.0
+
+# 删除zookeeper的日志,保留myid
+ls ./node-data/hadoop1/modules/zookeeper-3.4.5-cdh5.10.0/zkData | grep -v myid | xargs rm -rf {}
+ls ./node-data/hadoop2/modules/zookeeper-3.4.5-cdh5.10.0/zkData | grep -v myid | xargs rm -rf {}
+ls ./node-data/hadoop3/modules/zookeeper-3.4.5-cdh5.10.0/zkData | grep -v myid | xargs rm -rf {}
+
+
