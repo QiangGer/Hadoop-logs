568620dcb91cf1ad6d06585499266204389bcb32
xcq <xcq5120@gmail.com>
2020-04-08 16:35:46 +0800

    简单的spark demo，后续的web工程也在此目录下

---
+++ sparkWeb/src/main/scala/xyz/xcq5120/test/Test.scala
@@ -0,0 +1,32 @@
+package xyz.xcq5120.test
+
+import org.apache.spark.sql.SparkSession
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder
+      .master("yarn-cluster")
+      .appName("SparkTest")
+      .getOrCreate()
+
+    val filePath = args(0)
+//    val filePath = "./src/main/resource/word.input"
+
+//    val rdd = spark.sparkContext.textFile(filePath)
+//    val lines = rdd.flatMap( x => x.split(" "))
+//      .map(x => (x,1))
+//      .reduceByKey((a,b) => (a+b))
+//      .collect().toList
+//    println(lines)
+
+    import spark.implicits._
+    spark.read.textFile(filePath)
+      .flatMap(x=>x.split(" "))
+      .map(x => (x,1))
+      .groupBy("_1")
+      .count()
+      .show()
+
+  }
+}
